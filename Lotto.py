ed2fd0f73f4a12a7e5458366e8c321e40de47078
Patryk Tatarczuch <123479845+PatrykTatarczuch@users.noreply.github.com>
2023-10-07 20:43:12 +0200

    Update Lotto.py

--- Lotto.py
+++ Lotto.py
@@ -7,53 +7,49 @@ x = 1
 list_cor = 0
 while x <= len(list):
     x += 1
-    print("Wylosuj liczbę w przedziale od 1 do 49: ")
+    print("Write a number between 1 and 49")
     try:
         loss = int(input())
     except ValueError:
-        print("Podana wartość musi byc liczbą całkowitą")
+        print("The value provided must be an integer")
         x -= 1
         continue
     if loss in list_loss:
-        print("Liczby nie mogą się powtarzać")
+        print("Numbers cannot be repeated")
         x -= 1
     elif loss < 1:
         x -= 1
-        print("Wybierz liczbę o większej wartości")
+        print("Select a number with a higher value")
     elif loss > 49:
         x -= 1
-        print("Wybierz liczbę o mniejszej wartości")
+        print("Select a number with a lower value")
     elif 1 <= loss <= 49 and list[0] != loss and list[1] != loss and list[2] != loss and list[3] != loss and list[4] != loss and list[5] != loss:
-        print("Wylosowana liczba:", loss)
+        print("The entered number:", loss)
         list_loss += [loss]
     else:
         if loss not in list_loss:
             list_loss += [loss]
             list_cor += 1
-            print("Wylosowana liczba:", loss)
+            print("The entered number:", loss)
 
-
-
-print("Twoje liczby: ", sorted(list_loss))
-print("Liczby w puli: ", list)
-print("Wydałeś 3zł na kupon")
-if list_cor == 1:
-    print("Udało Ci się zgadnąć", list_cor, "liczbę")
-    print("Niestety nie udało Ci się nic zarobić")
-elif list_cor == 0:
-    print("Niestety nie udało Ci się zgadąć ani jednej liczby :(")
-    print("Następnym razem Ci się uda")
-elif list_cor == 6 or list_cor == 5:
-    print("Udało Ci się zgadnąć", list_cor, "liczb")
-    if list_cor == 6:
-        print("Wow!!! Zarobiłeś 7.000.000 milionów zł")
-    elif list_cor == 5:
-        print("Wow!!! Zarobiłeś 6.000 zł")
+print("Your numbers", sorted(list_loss))
+print("Numbers in the pool:", list)
+print("You spent PLN 3 on a coupon")
+if list_cor == 0:
+    print("Unfortunately, you failed to guess the number :(")
+    print("Next time you will succeed")
+elif list_cor == 1:
+    print("You guessed", list_cor, "number")
+    print("it will be better next time")
 else:
-    print("Udało Ci się zgadnąć", list_cor, "liczby")
-    if list_cor == 3:
-        print("Nieźle! Zarobiłeś 24 zł")
+    print("You guessed", list_cor, "number")
+    if list_cor == 6:
+        print("Wow!!! You earned PLN 7,000,000 million")
+    elif list_cor == 5:
+        print("Wow!!! You earned PLN 6,000")
     elif list_cor == 4:
-        print("Super! Zarobiłeś 205 zł")
+        print("Great! You earned PLN 205")
+    elif list_cor == 3:
+        print("Not bad! You earned PLN 24")
     else:
-        print("Byłeś bardzo blisko wygranej!")
+        print("You were very close to winning!")
